ef549a1e9ad8f2c79ed9c8d25da4f00ffda607a3
Zion Perez <zionperez@gmail.com>
2019-06-26 11:46:34 +0900

    Add moonsharp plugin. Implement LuaHelloWorld script.

---
+++ Assets/Scripts/LuaEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using MoonSharp.Interpreter;
+
+public class LuaEnvironment : MonoBehaviour
+{
+    private Script environment;
+    void Start()
+    {
+        Script.DefaultOptions.DebugPrint = (s) => Debug.Log(s);
+        environment = new Script();
+
+        environment.DoString("print 'Hello world!'");
+    }
+}
