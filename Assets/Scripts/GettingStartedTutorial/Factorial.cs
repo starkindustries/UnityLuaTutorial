6b4eadaf06d1ed420dc4b603f89a643306a4a619
Zion Perez <zionperez@gmail.com>
2019-07-08 15:37:17 +0900

    Implement part of the tutorials from the moonsharp website. Implement an Input field that takes lua script and runs it!

---
+++ Assets/Scripts/GettingStartedTutorial/Factorial.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonSharp.Interpreter;
+using System;
+
+public class Factorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Factorial (3): " + MoonSharpFactorial());
+        Debug.Log("Factorial2 (4): " + MoonSharpFactorial2());
+        Debug.Log("CallbackTest (5): " + CallbackTest());
+    }
+
+    // https://www.moonsharp.org/getting_started.html
+    // https://www.moonsharp.org/tutorial2.html
+    private double MoonSharpFactorial()
+    {
+        string scriptCode = @"    
+		-- defines a factorial function
+		function fact (n)
+			if (n == 0) then
+				return 1
+			else
+				return n*fact(n - 1)
+			end
+		end
+
+		return fact(mynumber)";
+
+        Script script = new Script();
+        script.Globals["mynumber"] = 3;
+        DynValue res = script.DoString(scriptCode);
+        return res.Number;
+    }
+
+    double MoonSharpFactorial2()
+    {
+        string scriptCode = @"    
+		-- defines a factorial function
+		function fact (n)
+			if (n == 0) then
+				return 1
+			else
+				return n*fact(n - 1)
+			end
+		end";
+
+        Script script = new Script();
+
+        script.DoString(scriptCode);
+
+        DynValue res = script.Call(script.Globals["fact"], 4);
+
+        return res.Number;
+    }
+
+    private static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    private static double CallbackTest()
+    {
+        string scriptCode = @"    
+        -- defines a factorial function
+        function fact (n)
+            if (n == 0) then
+                return 1
+            else
+                return Multiply(n, fact(n - 1));
+            end
+        end";
+
+        Script script = new Script();
+        script.Globals["Multiply"] = (Func<int, int, int>)Multiply;
+        script.DoString(scriptCode);
+        DynValue res = script.Call(script.Globals["fact"], 5);
+        return res.Number;
+    }    
+}
---
+++ Assets/Scripts/GettingStartedTutorial/LuaEditor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using MoonSharp.Interpreter;
+
+public class LuaEditor : MonoBehaviour
+{
+    public TextMeshProUGUI tmpTextField;
+    public TextMeshProUGUI tmpConsole;
+
+    private void Start()
+    {
+        // Script.DefaultOptions.DebugPrint = (s) => Debug.Log(s);
+        Script.DefaultOptions.DebugPrint = (s) => ConsoleLog(s);
+    }
+
+    private void HelloWorld()
+    {
+        Debug.Log("Hello world");
+    }    
+
+    private void ConsoleLog(string s)
+    {
+        tmpConsole.text += s + "\n";
+    }
+
+    public void DidPressRunButton()
+    {
+        Debug.Log(tmpTextField.text);
+        string scriptCode = tmpTextField.text;
+        if (scriptCode[scriptCode.Length-1] == (char)8203)
+        {
+            Debug.Log("Removing zero-width space found at end of script.");
+            scriptCode = scriptCode.Remove(scriptCode.Length - 1, 1);
+        }        
+
+        /*  
+        scriptCode = @"
+        -- print hello world
+        print('Hello WORLD')
+        ";
+        */
+        
+        Script script = new Script();
+
+        Debug.Log("Double quote: " + (int)'"');
+        // Zero-width space
+        // https://stackoverflow.com/questions/2973698/whats-html-character-code-8203        
+        Debug.Log("zero-width space: \"" + (char)8203 + "\"");
+
+        foreach(char c in scriptCode.Trim())
+        {
+            Debug.Log("\"" + c + "\": " + (int)c);
+        }
+
+        // script.Globals["Hello"] = (Func<>)HelloWorld;
+        script.DoString(scriptCode);
+        // DynValue res = script.Call(script.Globals["fact"], 5);
+
+    }
+}
